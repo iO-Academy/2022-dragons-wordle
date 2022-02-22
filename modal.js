99bd35788cda98ac135f1e171fa2dd0b56e90e14
Marcus Drury <marcusdrury@outlook.com>
2022-02-22 14:12:04 +0000

    Added JS event listeners and removed unnecessary classes from HTML

--- modal.js
+++ modal.js
@@ -1,1 +1,19 @@
+let modal = document.querySelector('.overlay')
+let modalClose = document.querySelector('.closeBtn')
+let instructionsBtn = document.querySelector('.instructions')
 
+instructionsBtn.addEventListener('click', (e) => {
+    modal.style.display = 'flex';
+    console.log('first')
+})
+
+modalClose.addEventListener('click', (e) => {
+    modal.style.display = 'none'
+    console.log('third')
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+})
