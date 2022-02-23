40c5cc7c2bbe7b4dd80bdc1e9a2cfe80e3e80342
Grace Palacz <gracelizbuckley@gmail.com>
2022-02-23 09:36:58 +0000

    removed semi colon from modal, added json file, fetched file

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const index = document.querySelector('.overlay')
 const modalClose = document.querySelector('.closeBtn')
 const instructionsBtn = document.getElementById('instructions')
 
+fetch('words.json')
+    .then((data)=> {
+        return data.json()
+    }).then((result) => {
+        console.log(result)
+})
+
 instructionsBtn.addEventListener('click', (e) => {
     index.style.display = 'flex';
 })
