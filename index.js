e6478313520b2105d36d133dd4652f7fd2bcdcdf
Marcus Drury <marcusdrury@outlook.com>
2022-02-24 11:21:18 +0000

    Added functionality to the retry button

--- index.js
+++ index.js
@@ -57,6 +57,10 @@ function addTileRow() {
 function buttonSwitchToRetry () {
     document.querySelector('.submitFormButton').style.display = 'none'
     document.querySelector('.retryButton').style.display = 'block'
+
+    document.querySelector('.retryButton').addEventListener('click', () => {
+        window.location.reload()
+    })
 }
 
 fetch('words.json')
